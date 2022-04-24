b5cffd5392b43349679282752f63ec1419393e22
glouarn <gaetan.louarn@inrae.fr>
2022-04-24 23:38:39 +0200

    Add sim and obs example files

--- data-raw/mydataset.R
+++ data-raw/mydataset.R
@@ -31,12 +31,19 @@ paramsd_0 <- read.table(unz(path_zip0, "paramsd_0.csv"), header=T, sep=";")
 ## exemple files
 path_totocompet <- system.file("extdata", "dtoto_ind_compet_exemple.csv", package = "RPackVGL")
 tabtoto_compet <- read.table(path_totocompet, header=T, sep=",", dec=".")
-#j'ai du le copier manuellement
+
+path_totosim <- system.file("extdata", "dtoto_sim_exemple.csv", package = "RPackVGL")
+tabtoto_sim <- read.table(path_totosim, header=T, sep=";", dec=".")
+
+path_obs <- system.file("extdata", "DigitLuz10_obs.xls", package = "RPackVGL")
+obs_exemple <- read_excel(path_obs, sheet="DigitLuz10_LD_Orca")
+
+
 
 # This should be the last line.
 # Note that names are unquoted.
 # I like using overwrite = T so everytime I run the script the
 # updated objects are saved, but the default is overwrite = F
-usethis::use_data(plt_par, toto_0, outHR_0, tabtoto_compet, overwrite = T)
+usethis::use_data(plt_par, toto_0, outHR_0, tabtoto_compet, tabtoto_sim ,obs_exemple, overwrite = T)
 
 
